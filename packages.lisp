533ba37bc19fffe4cce5af91b2f9dc5fc9f8b73f
Paul M. Rodriguez <pmr@ruricolist.com>
2014-06-13 19:48:34 -0500

    Use :magick as a nickname.

--- packages.lisp
+++ packages.lisp
@@ -29,9 +29,9 @@
 (in-package :cl-user)
 
 (defpackage :at.nil.lisp-magick
-  (:nicknames :lisp-magick)
+  (:nicknames :lisp-magick :magick)
   (:use :common-lisp)
   (:export :quantum-8 :quantum-16 :quantum-32 :quantum-64
            :byte->quantum :quantum->byte
-	   :with-pixel-wand :with-drawing-wand :with-magick-wand :give-wand
-	   :with-pixel-data :pixel :get-pixel :set-pixel))
+           :with-pixel-wand :with-drawing-wand :with-magick-wand :give-wand
+           :with-pixel-data :pixel :get-pixel :set-pixel))
