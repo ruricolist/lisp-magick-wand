4cec752035b648032614fe25ddf73e04cfec27ea
Paul M. Rodriguez <pmr@ruricolist.com>
2014-06-13 19:47:41 -0500

    Apply patch from Didier Verna.

--- option.lisp
+++ option.lisp
@@ -32,14 +32,14 @@
   (mnemonic magick-string)
   (type :long))
 
-(defmagickfun "GetMagickOptions" (:array magick-string/free nil :err-val nil)
+(defmagickfun "GetCommandOptions" (:array magick-string/free nil :err-val nil)
   ((table :int)))
-(defmagickfun "ParseMagickOption" :long
+(defmagickfun "ParseCommandOption" :long
   ((table :int) (list-p :boolean) (options magick-string)))
 
 (defparameter *list-options*
   (loop for i from 0 upto 100
-     for opts = (get-magick-options i)
+     for opts = (get-command-options i)
      when (and (member "List" opts :test #'equal)
 	       (member "Orientation" opts :test #'equal))
      return i
@@ -61,7 +61,7 @@
 
 (defun find-magick-option (table options)
   (dolist (opt (if (listp options) options (list options)))
-    (let ((n (parse-magick-option table nil opt)))
+    (let ((n (parse-command-option table nil opt)))
       (unless (eql n -1)
 	(return n)))))
 
@@ -70,10 +70,10 @@
 		(error "cannot get info about ~s" option-name))))
     (let ((names (remove-if #'(lambda (s) (and (eql (length s) 1)
 					       (digit-char-p (char s 0))))
-			    (remove-duplicates (get-magick-options id) :test #'equal))))
+			    (remove-duplicates (get-command-options id) :test #'equal))))
       (unless names
 	(error "unable to get option names for ~s/~s" option-name id))
       `(cffi:defcenum ,lisp-name
 	 ,@(loop for name in names
 		collect (list (magick-name-to-lisp-name name :keyword)
-			      (parse-magick-option id nil name)))))))
+			      (parse-command-option id nil name)))))))
